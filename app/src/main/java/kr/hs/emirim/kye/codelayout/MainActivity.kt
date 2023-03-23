71d27427837bd6dc289cad6a36f7ed0d29150d0c
Uque1013 <kye6735@naver.com>
2023-03-23 19:51:17 +0900

    Event Programming

--- app/src/main/java/kr/hs/emirim/kye/codelayout/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kye/codelayout/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.hs.emirim.kye.codelayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn1 :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1 = findViewById<Button>(R.id.btn1)
+
+        btn1.setOnClickListener {
+            Toast.makeText(applicationContext, "버튼을 눌렀어요~",Toast.LENGTH_SHORT).show()
+        }
     }
 }
