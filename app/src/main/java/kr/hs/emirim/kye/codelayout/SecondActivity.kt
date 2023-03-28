d9205d9e328e3ad7a92db67327181f54e67fb75a
Uque1013 <kye6735@naver.com>
2023-03-28 19:47:30 +0900

    버튼 4개 생성 및 링크 연결, dimen 파일 생성

--- app/src/main/java/kr/hs/emirim/kye/codelayout/SecondActivity.kt
+++ app/src/main/java/kr/hs/emirim/kye/codelayout/SecondActivity.kt
@@ -1,11 +1,46 @@
 package kr.hs.emirim.kye.codelayout
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var btn4 : Button
+    private fun Intent(actionView: String, parse: Uri?, s: String) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+
+        btn1.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"))
+            startActivity(intent)
+        }
+
+        btn2.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/010-1234-5678"))
+            startActivity(intent)
+        }
+
+        btn3.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/meida"))
+            startActivity(intent)
+        }
+
+        btn4.setOnClickListener {
+            finish()
+        }
     }
 }
